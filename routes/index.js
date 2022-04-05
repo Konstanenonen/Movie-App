db55dbbfb06d3637f63010dc10c8b9976c16811c
Konsta Nenonen <konstanenonen@protonmail.com>
2022-04-06 01:25:42 +0300

    Basic functionality is beginnig to form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const { default: axios } = require("axios");
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
-});
+axios
+  .get("https://www.omdbapi.com/?s=lord+of+the+rings&apikey=b400a9b")
+  .then((res) => {
+    const data = res.data.Search;
+    const presentableData = data
+      .map(
+        (movie) =>
+          `<tr class="table">
+      <td>${movie.Title}</td>
+      <td>${movie.Year}</td>
+      <td>${movie.Type}</td>
+      <td><img width=200 height=auto src="${movie.Poster}" /></td>
+      </tr>`
+      )
+      .reduce((pre, cur) => pre + cur);
+
+    const table = `<table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Title</th>
+        <th scope="col">Year</th>
+        <th scope="col">Type</th>
+        <th scope="col">Poster</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${presentableData}
+    </tbody>
+    </table>`;
+
+    /* GET home page. */
+    router.get("/", function (req, res, next) {
+      res.render("pages/index", { title: "Movie App", table: table });
+    });
+  });
 
 module.exports = router;
